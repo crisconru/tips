04615b6f9d1cab0ed1d7318c757f11eb0e9a30f9
Cristobal Contreras Rubio <crisconru@gmail.com>
2022-11-27 19:08:01 +0100

    entendiendo como  funciona el contexto con typescript

---
+++ src/web/frontend/react/useContext-TS/src/App.tsx
@@ -0,0 +1,13 @@
+import { Todo } from './todo/Todo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-header">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
